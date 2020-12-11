9ff7dc42fe2483d43ae4363b2b58e69c16fb3881
Rafael Flores <fita500@gmail.com>
2020-12-10 19:28:08 -0600

    Add promises tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,31 @@ test('Probar callback', () => {
         expect(str).toBe('aloH')
     })
 })
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if(!str) {
+            reject(Error('Error'))
+        }
+
+        resolve(str.split("").reverse().join(""))
+    })
+}
+
+test('Probar promesa', () => {
+    return reverseString2('Hola')
+        .then(string => {
+            expect(string).toBe('aloH')
+        })
+})
+
+test('Probar async/await', async () => {
+    const string = await reverseString2('Hola')
+    expect(string).toBe('aloH')
+})
+
+afterEach(() => console.log('Despues de cada prueba'))
+afterAll(() => console.log('Despues de todas las pruebas'))
+
+beforeEach(() => console.log('Antes de cada prueba'))
+beforeAll(() => console.log('Antes de todas las pruebas'))
